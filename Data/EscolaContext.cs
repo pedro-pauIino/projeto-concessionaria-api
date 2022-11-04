2acf51447e372ad25f7e78a31fea54dafb979be7
Pedro Paulino <pedrogpaulino@gmail.com>
2022-11-04 20:27:38 -0300

    atualizacao

---
+++ Data/EscolaContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoEscola_API.Models;
+
+
+namespace ProjetoEscola_API.Data
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext(DbContextOptions<EscolaContext> options) : base(options)
+        {
+        }
+        public DbSet<Aluno>? Aluno { get; set; }
+        public DbSet<Curso>? Curso { get; set; }
+        public DbSet<User>? Usuario { get; set; }
+    }
+}
